04efb7fb9a25fd348c725393d31355fb02ed964d
NaoufelBY <naoufel.benyahia@gmail.com>
2022-02-07 21:27:12 +0100

    day1

---
+++ src/main/java/com/smart2dotech/workshop1/Workshop1Application.java
@@ -0,0 +1,14 @@
+package com.smart2dotech.workshop1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshop1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshop1Application.class, args);
+		System.out.println ("hello");
+	}
+
+}
