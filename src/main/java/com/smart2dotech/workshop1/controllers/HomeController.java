06258663c4516a08e7046daf7c881868b159d2f3
NaoufelBY <naoufel.benyahia@gmail.com>
2022-02-08 22:22:16 +0100

    ajout css

--- src/main/java/com/smart2dotech/workshop1/controllers/HomeController.java
+++ src/main/java/com/smart2dotech/workshop1/controllers/HomeController.java
@@ -1,4 +1,6 @@
 package com.smart2dotech.workshop1.controllers;
+import java.util.ArrayList;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,6 +11,11 @@ public class HomeController {
 	@RequestMapping("/information") // ce que je tape dans l'url
 	public String info(Model model)
 	{
+		ArrayList<String> names= new ArrayList<>();
+		names.add("OCA");
+		names.add("OCP");
+		names.add("spring");
+		model.addAttribute("List", names);
 		String formation="formation fullstack00";
 		System.out.println("hoxddsdsdoo");
 		model.addAttribute("test",formation);
